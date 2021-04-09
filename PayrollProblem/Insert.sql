dda3125a82f9e1b83b1b26ea880af6d1e1664686
prashikjaware <prashikjaware123@gmail.com>
2021-04-09 11:13:46 +0530

    [Prashik]Added: Insert data values for each column with multiple rows

---
+++ PayrollProblem/Insert.sql
@@ -0,0 +1,5 @@
+use Payroll_Service
+insert into employee_payroll (name, salary, start) values
+('Prashik', 100000.0, '2018-01-03'),
+('Rani', 200000.0, '2018-01-03'),
+('Sita', 10000.0, '2020-05-21');
