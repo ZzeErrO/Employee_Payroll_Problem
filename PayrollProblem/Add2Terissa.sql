ced6533c514a4a5ff988ed8fa6449ccaa263fadc
prashikjaware <prashikjaware123@gmail.com>
2021-04-17 12:31:23 +0530

    [Prashik]Added: Add 2 Terissa with Department as Sales and Marketing

---
+++ PayrollProblem/Add2Terissa.sql
@@ -0,0 +1,5 @@
+use payroll_service
+
+insert into employee_payroll values
+('Terissa', 254035, '2021-1-12', 'F', 616544444, 'costarica', 'Sales', 519, 6395, 2156, 541656),
+('Terissa', 259598, '2012-1-12', 'F', 616533333, 'costarica', 'Marketing', 501, 6496, 2356, 542535)
